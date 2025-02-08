4cb17cb0538cefeb690fa230729eff9526185b5d
0x1eef <0x1eef@protonmail.com>
2025-02-08 02:00:43 -0300

    Add /etc/quranbot/quranbot.json.sample

--- libexec/quranbot/scripts/post-reminder.ts
+++ libexec/quranbot/scripts/post-reminder.ts
@@ -1,23 +1,43 @@
 import process from "node:process";
+import { readFile } from "node:fs/promises";
+import { resolve } from "node:path";
 import { createRestAPIClient } from "npm:masto"
 
 const post = async (masto, url: string) => {
-  const status = url +
-                 " \n\n " +
-                 "#TheNobleQuran #TheQuran #Islam #Islamic";
-  return await masto.v1.statuses.create({status});
+  try {
+    const hashtags = "#TheNobleQuran #TheQuran #Islam #Islamic";
+    const status = [url, "\n", hashtags].join("\n");
+    return await masto.v1.statuses.create({status});
+  } catch (err) {
+    return err;
+  }
 }
 
-(async () => {
-  const url   = process.argv[2];
-  const masto = createRestAPIClient({
-    url: "https://mastodon.social",
-    accessToken: process.env.access_token,
-  });
+const parse = async (path: string) => {
   try {
-    const res = await post(masto, url);
-    console.log("ok:", res.url);
-  } catch(e) {
-    console.error("error:", e);
+    const raw = await readFile(path);
+    return JSON.parse(raw.toString());
+  } catch (err) {
+    return err;
   }
-})();
+}
+
+async function main() {
+  const url  = process.argv[2];
+  const path = resolve(import.meta.dirname, "..", "..", "etc", "quranbot", "quranbot.json");
+  const etc  = await parse(path);
+  if (etc instanceof Error) {
+    console.error("quranbot:", "read or parse error");
+    process.exit(1);
+  }
+
+  const masto = createRestAPIClient(etc.mastodon);
+  const res = await post(masto, url);
+  if (res instanceof Error) {
+    console.error("quranbot:", "mastodon error");
+    process.exit(1);
+  }
+  console.log("quranbot:", res.url);
+}
+
+await main();
